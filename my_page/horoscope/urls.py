dd4846e9b2bfcb1044e002f9cfb50f9f5a682a89
visurm76 <visurm76@gmail.com>
2023-02-08 15:28:00 +0300

    Convert route in Django

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('<sign_zodiac>', views.get_info_about_sign_zodiac),
+    path('<int:sign_zodiac>', views.get_info_about_sign_zodiac_by_number),
+    path('<str:sign_zodiac>', views.get_info_about_sign_zodiac),
 ]
--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
-
 # Create your views here.
 
+zodiac_dict = {
+    'leo': 'Лев - пятый знако зодиака',
+    'scorpio': 'Скорпион - восьмой знак зодиака',
+    'taurus': 'Овен - первый знак зодиака'
+}
 
-def get_info_about_sign_zodiac(request, sign_zodiac):
-    if sign_zodiac == 'leo':
-        return HttpResponse('Знак зодиака Лев')
-    elif sign_zodiac == 'scorpio':
-        return HttpResponse('Знак зодиака Скорпион')
+
+def get_info_about_sign_zodiac(request, sign_zodiac: str):
+    description = zodiac_dict.get(sign_zodiac, None)
+    if description:
+        return HttpResponse(description)
     return HttpResponseNotFound(f"Неизвестный знак зодиака {sign_zodiac}")
+
+
+def get_info_about_sign_zodiac_by_number(request, sign_zodiac: int):
+    return HttpResponse(f"This is number {sign_zodiac}")
