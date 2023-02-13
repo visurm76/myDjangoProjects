63b0f7774fd60f5d8516be194d0c01ea727200ad
visurm76 <visurm76@gmail.com>
2023-02-13 19:02:27 +0300

    No end lesson

--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -13,11 +13,13 @@ zodiac_dict = {
 
 }
 
+def index(request):
+
 
 def get_info_about_sign_zodiac(request, sign_zodiac: str):
     description = zodiac_dict.get(sign_zodiac, None)
     if description:
-        return HttpResponse(description)
+        return HttpResponse(f'<h2>{description}</h2>')
     return HttpResponseNotFound(f"Неизвестный знак зодиака {sign_zodiac}")
 
 
@@ -25,5 +27,6 @@ def get_info_about_sign_zodiac_by_number(request, sign_zodiac: int):
     zodiacs = list(zodiac_dict)
     if sign_zodiac > len(zodiacs):
         return HttpResponseNotFound(f"Неправильный пороядковый номер {sign_zodiac}")
-    name_zodiac = zodiacs[sign_zodiac]
+    name_zodiac = zodiacs[sign_zodiac-1]
+    redirect_url = reverse('horoscope_name', args=[name_zodiac])
     return HttpResponseRedirect(f"/horoscope/{name_zodiac-1}")
